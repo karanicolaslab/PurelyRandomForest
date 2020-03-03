c6dff9e3acc37a9c67d2fd82a9003eb5acaa9f02
Shipra Malhotra <shprmalhotra23@gmail.com>
2020-03-03 18:19:17 -0500

    Add files via upload

---
+++ algorithms/PurelyRF.R
@@ -0,0 +1,177 @@
+### RF Regression ###
+
+## Randomly select a split point
+random_var <- function(x, y) {
+  ## No Gini index being involved
+  ## Randomly select one of the x values as split point
+  sortedx <- sort(unique(x[]))
+  n = length(sortedx) - 1
+  splitn <- sample(1:n,1)
+  split_at <- (sortedx[splitn] + sortedx[splitn + 1])/2
+  return(c(split = split_at))
+}
+random_var2 <- function(x, y) {
+  ## No Gini index being involved
+  ## Randomly select one of the x values as split point
+  splits <- sample(unique(x[]),1)
+  split_at <- splits[1]
+  return(c(split = split_at))
+}
+
+random_reg_tree <- function(formula, data, minsize) {
+  # A different random seed for each tree (as there is no dataset sampling)
+  sd = sample(1:10000,1)
+  print(sd)
+  set.seed(sd)
+  
+  # coerce to data.frame
+  data <- as.data.frame(data)
+  
+  # handle formula
+  formula <- terms.formula(formula)
+  
+  # get the design matrix
+  X <- model.matrix(formula, data)
+  
+  # extract target
+  y <- data[, as.character(formula)[2]]
+  
+  # initialize while loop
+  do_splits <- TRUE
+  
+  # create output data.frame with splitting rules and observations
+  tree_info <- data.frame(NODE = 1, NOBS = nrow(data), FILTER = NA, TERMINAL = "SPLIT",
+                           SPLIT = NA, stringsAsFactors = FALSE)
+  
+  # keep splitting until there are only leafs left
+  while(do_splits) {
+    
+    # which parents have to be splitted
+    to_calculate <- which(tree_info$TERMINAL == "SPLIT")
+    
+    for (j in to_calculate) {
+      
+      # handle root node
+      if (!is.na(tree_info[j, "FILTER"])) {
+        # subset data according to the filter
+        this_data <- subset(data, eval(parse(text = tree_info[j, "FILTER"])))
+        # get the design matrix
+        X <- model.matrix(formula, this_data)
+      } else {
+        this_data <- data
+      }
+      
+      # Splitting of a random point for each of the feature
+      # Randomly select one feature-split point
+      ##Sample a random variable
+      rv <- sample(c(2:9), 1)
+      Input <- data.frame(X[,rv])
+      colnames(Input) <- all.vars(formula)[(rv)]
+      splitting <- apply(Input,  MARGIN = 2, FUN = random_var, y = this_data[, all.vars(formula)[1]])
+      tmp_splitter <- splitting
+     
+      # define maxnode
+      mn <- max(tree_info$NODE)
+      
+      # paste filter rules
+      current_filter <- c(paste(names(tmp_splitter), ">=",
+                            splitting[names(tmp_splitter)]),
+                      paste(names(tmp_splitter), "<", 
+                            splitting[names(tmp_splitter)]))
+                            
+      # Error handling! check if the splitting rule has already been invoked
+      split_here  <- !sapply(current_filter,
+                             FUN = function(x,y) any(grepl(x, x = y)),
+                             y = tree_info$FILTER)
+      
+      # append the splitting rules
+      if (!is.na(tree_info[j, "FILTER"])) {
+        current_filter  <- paste(tree_info[j, "FILTER"], 
+                             current_filter, sep = " & ")
+      } 
+      
+      # calculate metrics within the children
+      metr <- lapply(current_filter,
+                         FUN = function(i, x, data, formula) {
+                          df <- subset(x = x, subset = eval(parse(text = i)))
+                          nobs <- nrow(df)
+                          w <- nobs/nrow(data)
+                          y <- df[, all.vars(formula)[1]]
+                          return(c(nobs))
+                         },
+                         x = this_data, data = data, formula = formula)  
+      
+      # extract relevant information
+      current_nobs <- sapply(metr, function(x) x[[1]])
+      current_y <- this_data[, all.vars(formula)[1]]
+      
+      
+      # insufficient minsize for split
+      if (any(current_nobs <= minsize)) {
+        split_here <- rep(FALSE, 2)
+      }
+      
+      # create children data frame
+      children <- data.frame(NODE = c(mn+1, mn+2),
+                             NOBS = current_nobs,
+                             FILTER = current_filter,
+                             TERMINAL = rep("SPLIT", 2),
+                             SPLIT = NA,
+                             row.names = NULL)[split_here,]
+      
+      # overwrite state of current node
+      tree_info[j, "TERMINAL"] <- ifelse(all(!split_here), "LEAF", "PARENT")
+      
+      if (tree_info[j, "TERMINAL"] == "PARENT") {
+      tree_info[j, "SPLIT"] <- names(tmp_splitter)
+      }
+      
+      # bind everything
+      tree_info <- rbind(tree_info, children)
+      
+      # check if there are any open splits left
+      do_splits <- !all(tree_info$TERMINAL != "SPLIT")
+    } # end for
+  } # end while
+  
+  # calculate fitted values and predictions at each node
+  pred <- data.frame()
+  fitted <-c()
+  for (i in seq_len(nrow(tree_info['TERMINAL']))) {
+      if (tree_info['TERMINAL']$TERMINAL[i] == "LEAF"){
+          ind <- as.numeric(rownames(subset(data, eval(parse(text = tree_info['FILTER']$FILTER[i])))))
+          fitted[ind] <- mean(y[ind])
+          prediction <- data.frame(cbind(i,mean(y[ind])))
+      }else{
+          if (i == 1){
+              prediction <- data.frame(cbind(i,mean(y[])))
+          }else{
+              ind <- as.numeric(rownames(subset(data, eval(parse(text = tree_info['FILTER']$FILTER[i])))))
+              prediction <- data.frame(cbind(i,mean(y[ind])))
+          }
+      }
+      pred <- rbind(pred, prediction)
+  }
+  # return everything
+  return(list(tree = tree_info, fit = fitted, nodepred = pred, formula = formula, data = data))
+}
+## Test Set Prediction
+prediction_test <- function(test, tree, nodepred) {
+    tfitted <-c()
+    for (i in seq_len(nrow(tree))) {
+        if (tree['TERMINAL']$TERMINAL[i] == "LEAF"){
+            ind <- as.numeric(rownames(subset(test, eval(parse(text = tree['FILTER']$FILTER[i])))))
+            tfitted[ind] <- nodepred[i,2]
+            #prediction <- data.frame(cbind(i,mean(y[ind])))
+        }else{
+            #if (i == 1){
+                #prediction <- data.frame(cbind(i,mean(y[])))
+            #}else{
+                #ind <- as.numeric(rownames(subset(test, eval(parse(text = trees$tree['FILTER']$FILTER[i])))))
+                #prediction <- data.frame(cbind(i,mean(y[ind])))
+            #}
+        }
+    }
+    
+    return(list(testfit = tfitted))
+}
---
+++ examples/FullyGrown_PurelyRF_example.R
@@ -0,0 +1,82 @@
+#### MAIN ####
+
+source("../algorithms/PurelyRF_fullgrown.R")
+
+set.seed(2579)
+
+ntable<-data.frame()
+test_table <-data.frame()
+
+### TRAIN and TEST datasets synthesis
+# Normally distrubted 10000 points with mean = 0 and SD = 1
+
+## TRAIN set
+a <- round(rnorm(10000, mean = 0, sd = 1),6)
+b <- round(rnorm(10000, mean = 0, sd = 1),6)
+c <- round(rnorm(10000, mean = 0, sd = 1),6)
+d <- round(rnorm(10000, mean = 0, sd = 1),6)
+e <- round(rnorm(10000, mean = 0, sd = 1),6)
+f <- round(rnorm(10000, mean = 0, sd = 1),6)
+g <- round(rnorm(10000, mean = 0, sd = 1),6)
+h <- round(rnorm(10000, mean = 0, sd = 1),6)
+
+data <- data.frame(cbind(a,b,c,d,e,f,g,h))
+colnames(data) <- c('A','B', 'C', 'D','E', 'F', 'G','H')
+
+data$I <- data$A + data$B + data$C + data$D + data$E + data$F + data$G + data$H
+
+eq <- I~A+B+C+D+E+F+G+H
+
+##TEST set
+ta <- round(rnorm(1000, mean = 0, sd = 1),4)
+tb <- round(rnorm(1000, mean = 0, sd = 1),4)
+tc <- round(rnorm(1000, mean = 0, sd = 1),4)
+td <- round(rnorm(1000, mean = 0, sd = 1),4)
+te <- round(rnorm(1000, mean = 0, sd = 1),4)
+tf <- round(rnorm(1000, mean = 0, sd = 1),4)
+tg <- round(rnorm(1000, mean = 0, sd = 1),4)
+th <- round(rnorm(1000, mean = 0, sd = 1),4)
+
+test <- data.frame(cbind(ta,tb,tc,td,te,tf,tg,th))
+colnames(test) <- c('A','B', 'C', 'D','E', 'F', 'G','H')
+
+## RANDOM FOREST
+## apply random_reg_tree function 100 times and each time it using a different seed
+## there is no sampling of any kind
+## minsize = 1 means the tree will be grown until there is a single data point on the leaf node.
+trees <- plyr::raply(
+  100,
+  random_reg_tree(formula = eq, data = data, minsize = 1),
+  .progress = "text",
+)
+# extract fit
+fits <- do.call("cbind", trees[,2])
+## second column is the fits column in trees output. This makes a table/dataset of size 10000 X 100 where each column is predicted label value from each tree
+
+# calculate the final fit as a mean of all regression trees
+rf_fit <- apply(fits, MARGIN = 1, mean, na.rm = TRUE)
+
+## Output
+ntable <- cbind(rf_fit,data$I)
+colnames(ntable)<-c('Xval','Yval')
+#print(ntable)
+write.table(ntable, "Performance_Training_Set_nodesize_1", quote = FALSE, row.names = FALSE, col.names = TRUE)
+
+## Testing phase
+## Get predictions from each tree
+## Final prediction for test set is the mean of all tree predictions
+
+result <- array()
+for (n in seq(1:100)) {
+    result[n] <- prediction_test(test = test, tree = trees[n,]$tree, nodepred = trees[n,]$nodepred)
+}
+
+testpreds <- do.call("cbind", result)
+pred <- apply(testpreds, MARGIN = 1, mean, na.rm = TRUE)
+
+# Ground Truth
+Test_GT <- test$A + test$B + test$C + test$D + test$E + test$F + test$G + test$H
+test_table <- cbind(pred, Test_GT)
+colnames(test_table)<-c('Xval','Yval')
+
+write.table(test_table, "Performance_Test_Set_nodesize_1", quote = FALSE, row.names = FALSE, col.names = TRUE)
